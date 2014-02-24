a9df31ebd849d704d3008a87881912394ea626e7
Pedro Santos <github@mariodelrio.com>
2014-02-24 01:34:53 +0000

    Added program tests to test post scenario.

--- ConsoleTwiterTests/ProgramTests.cs
+++ ConsoleTwiterTests/ProgramTests.cs
@@ -9,37 +9,70 @@ namespace ConsoleTwiterTests
     [TestFixture]
     public class ProgramTests
     {
+        private IConsole consoleMock;
+        private IInputParser parserMock;
+        private IRepository repositoryMock; 
+        private IMessageBroker brokerMock;
+        private IWall userWallMock;
+
+        private User bob;
+
+        [SetUp]
+        public void SetUp()
+        {
+            consoleMock = Substitute.For<IConsole>();
+            repositoryMock = Substitute.For<IRepository>(); 
+            userWallMock = Substitute.For<IWall>();
+            parserMock = Substitute.For<IInputParser>();
+            brokerMock = Substitute.For<IMessageBroker>();
+
+            bob = new User("Bob", userWallMock);
+        }
+
         [Test]
         public void GivenTheUserTypesACommandWhenTheProgramReadsTheCommandThenItInvokesParseOnInputParser()
         {
-            var console = Substitute.For<IConsole>();
-            var parser = Substitute.For<IInputParser>();
+            consoleMock.ConsoleRead().Returns("user input");
 
-            console.ConsoleRead().Returns("user input");
-
-            var program = new Program(console, parser);
+            var program = new Program(consoleMock, parserMock);
 
             program.Start();
 
-            parser.Received().Parse("user input");
+            parserMock.Received().Parse("user input");
         }
 
         [Test]
         public void GivenTheUserTypesAPostCommandWhenTheProgramExecutesThenItCallsPostOnMessageBroker()
         {
-            var console = Substitute.For<IConsole>();
-            var broker = Substitute.For<IMessageBroker>();
+            var commandFactory = new CommandFactory(brokerMock);
+            var parser = new InputParser(commandFactory);
+
+            consoleMock.ConsoleRead().Returns("Bob -> my message");
+
+            var program = new Program(consoleMock, parser);
+
+            program.Start();
+
+            brokerMock.Received().Post("Bob", "my message");
+        }
+            
+        [Test]
+        public void GivenTheUserTypesAPostCommandWhenTheProgramExecutesThenItCallsPostOnUserWall()
+        {
+            var broker = new MessageBroker(repositoryMock);
 
             var commandFactory = new CommandFactory(broker);
             var parser = new InputParser(commandFactory);
 
-            console.ConsoleRead().Returns("Bob -> my message");
+            consoleMock.ConsoleRead().Returns("Bob -> my message");
 
-            var program = new Program(console, parser);
+            repositoryMock.FindByIdentifier("Bob").Returns(bob);
+
+            var program = new Program(consoleMock, parser);
 
             program.Start();
 
-            broker.Received().Post("Bob", "my message");
+            userWallMock.Received().Post("my message");
         }
     }
 }
