6acda3769384f1a3922b6d37328bf0d4ff524702
Pedro Santos <github@mariodelrio.com>
2014-02-24 15:01:47 +0000

    Code cleanup.

--- ConsoleTwitter/Messages/OutputMessageFormater.cs
+++ ConsoleTwitter/Messages/OutputMessageFormater.cs
@@ -11,41 +11,49 @@ namespace ConsoleTwitter
 
         private string ElapsedTime(DateTime date) 
         {  
-            int intYears = SystemTime.Now().Year - date.Year; 
-            int intMonths = SystemTime.Now().Month - date.Month; 
-            int intDays = SystemTime.Now().Day - date.Day; 
-            int intHours = SystemTime.Now().Hour - date.Hour; 
-            int intMinutes = SystemTime.Now().Minute - date.Minute; 
-            int intSeconds = SystemTime.Now().Second - date.Second; 
+            int elapsedYears = SystemTime.Now().Year - date.Year; 
 
-            if (intYears > 0)
+            if (elapsedYears > 0)
             {
-                return String.Format("{0} {1} ago", intYears, (intYears == 1) ? "year" : "years");
+                return String.Format("{0} {1} ago", elapsedYears, (elapsedYears == 1) ? "year" : "years");
             } 
-            else if (intMonths > 0)
+
+            int elapsedMonths = SystemTime.Now().Month - date.Month; 
+             
+            if (elapsedMonths > 0)
             {
-                return String.Format("{0} {1} ago", intMonths, (intMonths == 1) ? "month" : "months");
+                return String.Format("{0} {1} ago", elapsedMonths, (elapsedMonths == 1) ? "month" : "months");
             } 
-            else if (intDays > 0)
+
+            int elapsedDays = SystemTime.Now().Day - date.Day; 
+
+            if (elapsedDays > 0)
             {
-                return String.Format("{0} {1} ago", intDays, (intDays == 1) ? "day" : "days");
+                return String.Format("{0} {1} ago", elapsedDays, (elapsedDays == 1) ? "day" : "days");
             } 
-            else if (intHours > 0)
+
+            int elapsedHours = SystemTime.Now().Hour - date.Hour; 
+
+            if (elapsedHours > 0)
             {
-                return String.Format("{0} {1} ago", intHours, (intHours == 1) ? "hour" : "hours");
+                return String.Format("{0} {1} ago", elapsedHours, (elapsedHours == 1) ? "hour" : "hours");
             } 
-            else if (intMinutes > 0)
+
+            int elapsedMinutes = SystemTime.Now().Minute - date.Minute; 
+
+            if (elapsedMinutes > 0)
             {
-                return String.Format("{0} {1} ago", intMinutes, (intMinutes == 1) ? "minute" : "minutes");
+                return String.Format("{0} {1} ago", elapsedMinutes, (elapsedMinutes == 1) ? "minute" : "minutes");
             } 
-            else if (intSeconds > 0)
+
+            int elapsedSeconds = SystemTime.Now().Second - date.Second;
+
+            if (elapsedSeconds > 0)
             {
-                return String.Format("{0} {1} ago", intSeconds, (intSeconds == 1) ? "second" : "seconds");
+                return String.Format("{0} {1} ago", elapsedSeconds, (elapsedSeconds == 1) ? "second" : "seconds");
             }
-            else 
-            { 
-                return "0 seconds ago";
-            } 
+
+            return "0 seconds ago";
         }
     }
 }
